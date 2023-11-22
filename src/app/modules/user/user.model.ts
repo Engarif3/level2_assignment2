ae6c1975e631e98d527a392234316895b9f3a3b1
Md Arifur Rahman <arif.aust.eng@gmail.com>
2023-11-22 18:30:18 +0100

    user schema and model done

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,9 +1,50 @@
 import { Schema, model } from 'mongoose';
-import { TUser } from './user.interface';
+import { TAddress, TFullName, TUser } from './user.interface';
+
+const fullNameSchema = new Schema<TFullName>({
+  firstName: { type: String, required: [true, 'First name is required'] },
+  lastName: { type: String, required: [true, 'Last name is required'] },
+});
+
+const addressSchema = new Schema<TAddress>({
+  street: { type: String, required: [true, 'First name is required'] },
+  city: { type: String, required: [true, 'First name is required'] },
+  country: { type: String, required: [true, 'First name is required'] },
+});
 
 const userNameSchema = new Schema<TUser>({
-  userId: { type: Number, required: [true, 'user id is required'] },
-  username: { type: String },
+  userId: {
+    type: Number,
+    required: [true, 'User id is required'],
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: [true, 'User name is required'],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+  },
+  fullName: fullNameSchema,
+  age: {
+    type: Number,
+    required: [true, 'Age is required'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+  },
+  isActive: {
+    type: Boolean,
+    required: true,
+  },
+  hobbies: {
+    type: [String],
+    required: [true, 'Hobby is required'],
+  },
+  address: addressSchema,
 });
 
 export const UserModel = model<TUser>('user', userNameSchema);
