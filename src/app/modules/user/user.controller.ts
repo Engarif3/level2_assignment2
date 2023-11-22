39c86a7d388a3aab95fd636b0a58e38dbbb45d65
Md Arifur Rahman <arif.aust.eng@gmail.com>
2023-11-22 20:18:49 +0100

    on user get route implimented  successfully

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from 'express';
 import { UserServices } from './user.service';
-// import { StudentServices } from './student.service';
 
 const createUser = async (req: Request, res: Response) => {
   try {
@@ -24,6 +23,21 @@ const createUser = async (req: Request, res: Response) => {
   }
 };
 
+const getAllUsers = async (req: Request, res: Response) => {
+  try {
+    const result = await UserServices.getAllUsersFromDB();
+
+    res.status(200).json({
+      success: true,
+      message: 'Users fetched successfully!',
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 export const UserControllers = {
   createUser,
+  getAllUsers,
 };
